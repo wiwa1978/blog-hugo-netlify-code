e12e2709a17d5271918817a1024823d4a61550f1
Wim Wauters <wauterw@cisco.com>
2021-01-25 10:29:37 +0100

    Initial commit

---
+++ Flask/Flask-Todo-Bootstrap/web/app/admin/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+
+from ..models import Task
+
+
+class TodoForm(FlaskForm):
+    """
+    Form for admin to add or edit a department
+    """
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
